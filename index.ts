117903607c331be61a08b4ebea3dcc5c935efd57
WDC <shaorin62@gmail.com>
2018-11-06 10:30:33 +0900

    Type4

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("hello");
+const name = "Shaorin",
+age = 34,
+gender = "male";
+
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are ${gender}`);
+}
+
+
+sayHi(name, age);
+
+
+export{};
