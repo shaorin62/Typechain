0efea008608cac325c25ffee5afc2fcd81499a5e
WDC <shaorin62@gmail.com>
2018-11-07 10:44:10 +0900

    interface

--- index.ts
+++ index.ts
@@ -1,14 +1,32 @@
+
+interface Human {
+    name : String;
+    gender : String;
+    age : number;
+}
+
 const name = "Shaorin",
 age = 34,
 gender = "male";
 
+const person = {
+    name : "shaorin",
+    gender : "male",
+    age : 34
+}
 
 const sayHi = (name, age, gender?) => {
     console.log(`Hello ${name}, you are ${age}, you are ${gender}`);
 }
 
+const sayHi2 = (person : Human) => {
+    console.log(`Hello ${person.name}, you are ${person.age}, you are ${person.gender}`);
+}
+
 
 sayHi(name, age);
+
+console.log(sayHi2(person));
 
 
 export{};
