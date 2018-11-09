6dbe4510a9eacdbf6dd73d65193580254bf15800
WDC <shaorin62@gmail.com>
2018-11-09 14:39:27 +0900

    initpage

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
     public index:number;
     public hash:string;
     public previousHash:string;
     public data:string;
     public timestamp:number;
+
+    static calculateBlockHash = (
+        index:number, 
+        previousHash:string, 
+        timestamp:number, 
+        data:string):string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(
         index:number,
         hash:string,
@@ -19,11 +28,19 @@ class Block {
     }
 }
 
+
 const genesisBlock:Block = new Block(0,"2131321","","Hello",123456);
 
 
 let blockchain: [Block] = [genesisBlock]
 
+const getBlockchain = () : Block[] => blockchain;
+
+const getLatestBlock = () : Block => getBlockchain[blockchain.length - 1];
+
+const getNewTimeStemp = () : number => Math.round(new Date().getTime()/1000);
+
 console.log(blockchain);
+
 
 export{};
